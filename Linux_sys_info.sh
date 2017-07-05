d943549830b98ee511af231f5fd1acf67741a8bd
SuriyaKanagaraj <suriyaraj91@gmail.com>
2017-07-05 14:21:49 +0530

    New linux script
    linux

---
+++ Linux_sys_info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hi welcome to jenkins"
+echo "---------------------"
+echo "This is a script to get information "
+echo "System memory"
+free -m
+echo "------------------------------------"
+echo " DIsk Usage"
+df -hT
+echo "------------------------------------"
+echo "Current date and time"
+date
